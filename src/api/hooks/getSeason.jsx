0b8449278dc32bb40b5bc0b52323dac5dd730d1e
Tranh <maithetranh@gmail.com>
2025-03-19 10:44:45 +0700

    season

--- src/api/hooks/getSeason.jsx
+++ src/api/hooks/getSeason.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import http from "../config/http";
 
-function getSeason() {
+function getSeason(option, year, season) {
   return ({} = useQuery({
-    queryKey: ["season-now"],
+    queryKey: [`season`, option],
     queryFn: async () => {
       return await http
-        .get("/seasons/now")
+        .get(`/seasons/${year}/${season}`, {
+          params: option,
+        })
         .then((res) => res.data.data)
         .catch((err) => console.log(err));
     },
--- src/pages/season/Season.jsx
+++ src/pages/season/Season.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useState } from "react";
+import getSeason from "../../api/hooks/getSeason";
+import Side from "../../components/layout/SideBar/Side";
 
 function Season() {
+  const [option, setOption] = useState({
+    sfw: true,
+  });
+  const handleChange = (e) => {
+    const selected = JSON.parse(e.target.value);
+    setOption((prev) => ({
+      ...prev,
+      ...selected,
+    }));
+  };
+  const yearSeason = [
+    {
+      year: 2025,
+      seasons: ["winter", "spring", "fall"],
+    },
+    {
+      year: 2024,
+      seasons: ["winter", "fall"],
+    },
+  ];
+
+  const { data: seasons, isLoading } = getSeason();
   return (
-    <div>Season</div>
-  )
+    <>
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-box">
+        <input
+          type="radio"
+          name="my_tabs_6"
+          className="tab"
+          aria-label="Tab 1"
+          defaultChecked
+        />
+        {yearSeason.map((item, index) => {
+          item.seasons.map((i, index) => (
+            <input
+              type="radio"
+              name="my_tabs_6"
+              className={`${item.year} ${i}`}
+              aria-label="Tab 1"
+            />
+          ));
+        })}
+      </div>
+      <div className="tabs tabs-border w-[90%] mx-auto">
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab"
+          aria-label="All"
+          value='{"filter": ""}'
+          onChange={handleChange}
+          defaultChecked
+        />
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab"
+          aria-label="TV"
+          value='{"filter": "tv"}'
+          onChange={handleChange}
+        />
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab"
+          aria-label="ONA"
+          value='{"filter": "ona"}'
+          onChange={handleChange}
+        />
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab"
+          aria-label="Movie"
+          value='{"filter": "movie"}'
+          onChange={handleChange}
+        />
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab"
+          aria-label="Special"
+          value='{"filter": "special"}'
+          onChange={handleChange}
+        />
+      </div>
+      {isLoading ? (
+        <div className="h-[100vh]"></div>
+      ) : (
+        <div className="lg:w-[85%] mx-auto py-3 flex flex-col md:flex-row gap-3">
+          {/* Left */}
+
+          {/* Right */}
+          {/* <Side /> */}
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Season
+export default Season;
